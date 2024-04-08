908535e9f6f32a3851018447576c7a12332cf441
Lorenzo Mondragon <lomondragon9127@gmail.com>
2024-04-07 18:12:30 -0600

    add ui

---
+++ CS464 Project/Assets/Scripts/Color Names.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorNames : MonoBehaviour
+{
+    public Text color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        color.text = "Red";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
