9de310533270e218acb232dfac3187a94fbeaa83
Lorenzo Mondragon <lomondragon9127@gmail.com>
2024-05-01 23:27:46 -0600

    update ar

--- CS464 Project (VR)/Assets/Scripts/ColorNames.cs
+++ CS464 Project (VR)/Assets/Scripts/ColorNames.cs
@@ -5,87 +5,103 @@ using TMPro;
 
 public class ColorNames : MonoBehaviour
 {
-    public TextMeshProUGUI colorName; 
+    public TextMeshProUGUI colorName;
 
     private Dictionary<string, Color> colorMap;
-    private List<string> colorKeys;
+    private List<string> colorNames;
+    private List<string> words;
     private int currentIndex = 0;
 
     void Start()
     {
-        colorMap = new Dictionary<string, Color>(){
-            { "Red", Color.blue },    // "Red" text will be displayed in blue color
-            { "Blue", Color.red },    // "Blue" text will be displayed in red color
-            { "Green", Color.yellow }, // "Green" text will be displayed in magenta color
-            { "Yellow", Color.blue },  // "Yellow" text will be displayed in green color
-            { "Red", Color.red },      // "Orange" text will be displayed in red color
-            { "Blue", Color.green },
-            { "Red", Color.red },    
-            { "Yellow", Color.green },
-            { "Green", Color.yellow},
-            { "Red", Color.green },
-            { "Blue", Color.red },
-            { "Yellow", Color.blue },
-            { "Green", Color.yellow },
-            { "Red", Color.blue },
-            { "Blue", Color.yellow },
-            { "Yellow", Color.red },
-            { "Green", Color.green },
-            { "Red", Color.yellow },
-            { "Blue", Color.green },
-            { "Yellow", Color.blue },
-            { "Green", Color.red },
-            { "Red", Color.red },
-            { "Blue", Color.blue },
-            { "Yellow", Color.yellow },
-            { "Green", Color.green },
-            { "Red", Color.blue },
-            { "Blue", Color.yellow },
-            { "Yellow", Color.red },
-            { "Green", Color.green },
-            { "Red", Color.yellow },
-            { "Blue", Color.green },
-            { "Yellow", Color.blue },
-            { "Green", Color.red },
-            { "Red", Color.red },
-            { "Blue", Color.blue },
-            { "Yellow", Color.yellow },
-            { "Green", Color.green },
-            { "Red", Color.green },
-            { "Blue", Color.red },
-            { "Yellow", Color.blue },
-            { "Green", Color.yellow },
-            { "Red", Color.blue },
-            { "Blue", Color.yellow },
-            { "Yellow", Color.red },
-            { "Green", Color.green }
-        };
+        colorMap = new Dictionary<string, Color>();
+        colorNames = new List<string>() { "Yellow", "Blue", "Green", "Red" };
+        words = GenerateWords();
 
-        colorKeys = new List<string>(colorMap.Keys); // Store the keys in a list for easy access
+        GenerateColorMap();
         StartCoroutine(CycleColors());
+    }
+
+    List<string> GenerateWords()
+    {
+        List<string> generatedWords = new List<string>();
+
+        foreach (string colorName in colorNames)
+        {
+            foreach (string wordColor in colorNames)
+            {
+                generatedWords.Add(colorName + " " + wordColor);
+            }
+        }
+
+        return generatedWords;
+    }
+
+    void GenerateColorMap()
+    {
+        // Shuffle the list of words
+        Shuffle(words);
+
+        // Assign colors to words
+        for (int i = 0; i < words.Count; i++)
+        {
+            string colorName = colorNames[i % colorNames.Count];
+            colorMap.Add(words[i], GetColor(colorName));
+        }
     }
 
     IEnumerator CycleColors()
     {
         while (true)
         {
-            DisplayColorName(colorKeys[currentIndex]);
-            currentIndex = (currentIndex + 1) % colorKeys.Count; // Loop back to the start after reaching the end
+            DisplayColorName(words[currentIndex]);
+            currentIndex = (currentIndex + 1) % words.Count; // Loop back to the start after reaching the end
             yield return new WaitForSeconds(3); // Wait for 3 seconds
         }
     }
 
-    void DisplayColorName(string colorNameString)
+    void DisplayColorName(string word)
     {
-        if (colorMap.TryGetValue(colorNameString, out Color textColor))
+        if (colorMap.TryGetValue(word, out Color textColor))
         {
-            colorName.text = colorNameString;
+            colorName.text = word;
             colorName.color = textColor;
         }
         else
         {
             colorName.text = "Unknown Color";
             colorName.color = Color.black; // Default to black if the color is not found
+        }
+    }
+
+    Color GetColor(string colorName)
+    {
+        switch (colorName)
+        {
+            case "Yellow":
+                return Color.yellow;
+            case "Blue":
+                return Color.blue;
+            case "Green":
+                return Color.green;
+            case "Red":
+                return Color.red;
+            default:
+                return Color.black; // Default to black if color name is not recognized
+        }
+    }
+
+    // Fisher-Yates shuffle algorithm for shuffling lists
+    void Shuffle(List<string> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            string value = list[k];
+            list[k] = list[n];
+            list[n] = value;
         }
     }
 }
